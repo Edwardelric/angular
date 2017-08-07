23da3c2adfab324d90aea72a09ea7e15a8b9fb85
Edward <838298008@qq.com>
2017-08-07 11:16:06 +0800

    feature_heroes01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>这里是标题的内容：{{title}}</h1>
+    <h2>这里是英雄的名称：{{hero}} details!</h2>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  hero = 'spiderman';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AsideComponent } from './aside/aside.component';
-import { MenuComponent } from './menu/menu.component';
-import { ContentComponent } from './content/content.component';
-import { FooterComponent } from './footer/footer.component';
-import { StockmanageComponent } from './stock/stockmanage/stockmanage.component';
-import { StarsComponent } from './stars/stars.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    AsideComponent,
-    MenuComponent,
-    ContentComponent,
-    FooterComponent,
-    StockmanageComponent,
-    StarsComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
