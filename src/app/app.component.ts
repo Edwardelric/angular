120f8f604bacb36b92eeed7a379ae250e62380d8
Edward <838298008@qq.com>
2017-08-07 16:22:43 +0800

    feature_angular-cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>这里是标题的内容：{{title}}</h1>
-    <h2>这里是英雄的名称：{{hero}} details!</h2>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
-  hero = 'spiderman';
 }
