cd7b52afbd99ddc5e5058f319d566fe14b9bba76
Edward <838298008@qq.com>
2017-08-07 11:20:09 +0800

    feature_heroes02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>这里是标题的内容：{{title}}</h1>
-    <h2>这里是英雄的名称：{{hero}} details!</h2>
+    <h2>这里是英雄的id：{{hero.id}} details!</h2>
+    <h2>这里是英雄的名称：{{hero.name}} details!</h2>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
-  hero = 'spiderman';
+  hero: Hero = {
+    id: 1,
+    name: 'spiderman'
+  }
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
